c0e42315b14e4eb183ea122b7cdb63861d4b3419
Charles M. Wolfe <79966004+CMWolfe922@users.noreply.github.com>
2022-02-07 17:06:16 -0600

    Created a write file script that writes out data from the file to a json file.

---
+++ demo_06/writefile-demo.js
@@ -0,0 +1,12 @@
+var fs = require('fs')
+
+// to write a file we use writeFile instead of readFile
+var data = {
+    name: 'Bob'
+}
+
+// This alone won't give us what we want because the
+// second parameter is expecting a string and we
+// are passing in a JSON object. Using Json.stringify we will have
+// convert our object over into a string.
+fs.writeFile('data.json', data)
