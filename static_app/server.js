08e8dc665009cbcd4082c1f38cb5c4a5862ef23e
Charles M. Wolfe <79966004+CMWolfe922@users.noreply.github.com>
2022-02-08 02:21:21 -0600

    Downloaded body-parser to package, added postMessage function
    I still can't get it to work. I can't find the bug

--- static_app/server.js
+++ static_app/server.js
@@ -1,11 +1,16 @@
 // Express doesn't come with node so I have to install it
 // Create a variable express and saved it to an instance of express
 var express = require('express');
+// now that I installed the body-parser package, I need to import it
+// so we can set it up as middleware.
+var bodyParser = require('body-parser');
 var app = express();
 
 // now to host a static file I will use app.use() function
 
 app.use(express.static(__dirname)) // tells express we will be hosting a static file
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
 
 //now we create another variable called messages
 var messages = [
@@ -16,6 +21,13 @@ var messages = [
 // from our front end and backend
 app.get('/messages', (req, res) => {
     res.send(messages)
+})
+
+app.post('/messages', (req, res) => {
+    // console.log(req.body)
+    // so now instead of using the console.log we will use
+    messages.push(req.body)
+    res.sendStatus(200)
 })
 
 
@@ -31,3 +43,5 @@ var server = app.listen(3000, () => {
 
 // NOW WE WILL WORK ON GETTING MESSAGES FROM THE FRONT END TO THE
 // BACKEND
+// So now we need to send the post request from our browser app not
+// the postman app.
