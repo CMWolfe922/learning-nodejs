8a162dbdd69043a2caa7c7154480b603fab7e0d5
Charles M. Wolfe <79966004+CMWolfe922@users.noreply.github.com>
2022-02-07 20:02:41 -0600

    Package updated, and created index.html

---
+++ static_app/server.js
@@ -0,0 +1,7 @@
+// Express doesn't come with node so I have to install it
+// Create a variable express and saved it to an instance of express
+var express = require('express');
+var app = express();
+
+// app is an instance of express and .listen tells it to listen on port 3000
+app.listen(3000)
