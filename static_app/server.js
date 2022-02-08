1f8885ca4d0cf1400e89a3baa34d38fb86a9c27d
Charles M. Wolfe <79966004+CMWolfe922@users.noreply.github.com>
2022-02-08 00:57:29 -0600

    Updated the server.js to have a messages variable and added an app.get callback function

--- static_app/server.js
+++ static_app/server.js
@@ -6,6 +6,19 @@ var app = express();
 // now to host a static file I will use app.use() function
 
 app.use(express.static(__dirname)) // tells express we will be hosting a static file
+
+//now we create another variable called messages
+var messages = [
+    {name: 'Tim', message: 'Hi'},
+    {name: 'Jane', message: 'Hello'},
+]
+// Here we will use the app.get function to handle getting the messages
+// from our front end and backend
+app.get('/messages', (req, res) => {
+    res.send(messages)
+})
+
+
 // app is an instance of express and .listen tells it to listen on port 3000
 // Now lets set a call back on app.listen
 var server = app.listen(3000, () => {
@@ -14,3 +27,7 @@ var server = app.listen(3000, () => {
     // the port by setting app.listen equal to a variable
     // in case the port is changed once we deploy the app on a server.
 })
+
+
+// NOW WE WILL WORK ON GETTING MESSAGES FROM THE FRONT END TO THE
+// BACKEND
