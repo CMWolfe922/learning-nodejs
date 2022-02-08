b640c769579bd6ac2339bfe4a8160e4102ea0931
Charles M. Wolfe <79966004+CMWolfe922@users.noreply.github.com>
2022-02-07 21:08:16 -0600

    Updated the notes and created the server variable in server.js

--- static_app/server.js
+++ static_app/server.js
@@ -5,6 +5,12 @@ var app = express();
 
 // now to host a static file I will use app.use() function
 
-app.use(express.static(__dirname))
+app.use(express.static(__dirname)) // tells express we will be hosting a static file
 // app is an instance of express and .listen tells it to listen on port 3000
-app.listen(3000)
+// Now lets set a call back on app.listen
+var server = app.listen(3000, () => {
+    console.log('Server is listening on port', server.address().port)
+    // now we can hard code the port or we can give reference to
+    // the port by setting app.listen equal to a variable
+    // in case the port is changed once we deploy the app on a server.
+})
