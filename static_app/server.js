26695069804cb00d276cd892dbaf3eab3d1ab2e4
Charles M. Wolfe <79966004+CMWolfe922@users.noreply.github.com>
2022-02-09 17:40:00 -0600

    Changed var = messages to message and changed the .get and .post param to message instead of messages. I also fixed the io issue.

--- static_app/server.js
+++ static_app/server.js
@@ -6,16 +6,19 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var app = express()
 /**
+ * Setting up socket.io is trickier than most packages because it needs
+ * to tie into express. So we will create a regular http server with node
+ * that will share with express and then socket.io
  * Now I need to create a socket.io connection so that the app can
  * listen on the server for new requests
  * and then create an io connection with http
  */
-var http = require('http').Server(app)
-var io  = require('socket.io')(http)
+const http = require('http').Server(app)
+const io  = require('socket.io')(http)
 
 // Now I will connect my MongoDB database to store the messages and
 // use that db to save and retrieve the messages that need to be posted.
-var mongoose  = require('mongoose')
+// var mongoose  = require('mongoose')
 // now to host a static file I will use app.use() function
 
 app.use(express.static(__dirname)) // tells express we will be hosting a static file
@@ -25,22 +28,22 @@ app.use(bodyParser.urlencoded({extended: false}))
 // Now I will give my mongodb connection url. Since this is just a test
 // db I don't care that the password is online. But for future uses,
 // it needs to be stored in a config file somewhere safe.
-var dbUrl = "mongodb+srv://dbconnector:yk0BTK65RmKH5iiO@practicecluster.wasnv.mongodb.net/test"
+// var dbUrl = "mongodb+srv://dbconnector:yk0BTK65RmKH5iiO@practicecluster.wasnv.mongodb.net/test"
 //now we create another variable called messages
-var messages = [
+var message = [
     {name: 'Tim', message: 'Hi'},
     {name: 'Jane', message: 'Hello'},
 ]
 // Here we will use the app.get function to handle getting the messages
 // from our front end and backend
 app.get('/messages', (req, res) => {
-    res.send(messages)
+    res.send(message)
 })
 
 app.post('/messages', (req, res) => {
     // console.log(req.body)
     // so now instead of using the console.log we will use
-    messages.push(req.body)
+    message.push(req.body)
     io.emit('message', req.body)
     res.sendStatus(200)
 })
@@ -49,13 +52,15 @@ io.on('connection', (socket) => {
     console.log('a user connected')
 })
 
-mongoose.connect(dbUrl, { useMongoClient: true }, (err) => {
-    console.log('mongo db connection', err)
-})
+// mongoose.connect(dbUrl, { useMongoClient: true }, (err) => {
+//     console.log('mongo db connection', err)
+// })
 
 // app is an instance of express and .listen tells it to listen on port 3000
 // Now lets set a call back on app.listen
-var server = app.listen(3000, () => {
+// Since we setup socket.io we had to change the listen code to the http server
+// we setup so that the browser can find the socet.io file
+var server = http.listen(3000, () => {
     console.log('Server is listening on port', server.address().port)
     // now we can hard code the port or we can give reference to
     // the port by setting app.listen equal to a variable
