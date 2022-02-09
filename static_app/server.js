0c5dc75b026c0b74cf320d0db703c5077b4a6b76
Charles M. Wolfe <79966004+CMWolfe922@users.noreply.github.com>
2022-02-09 16:40:53 -0600

    imported socket.io, mongoose and added them to the files
    I also, copy and pasted the example html file over mine because  kept getting errors and couldn't find it. Yet I am still getting errors so idk what is wrong.

--- static_app/server.js
+++ static_app/server.js
@@ -5,13 +5,27 @@ var express = require('express')
 // so we can set it up as middleware.
 var bodyParser = require('body-parser')
 var app = express()
+/**
+ * Now I need to create a socket.io connection so that the app can
+ * listen on the server for new requests
+ * and then create an io connection with http
+ */
+var http = require('http').Server(app)
+var io  = require('socket.io')(http)
 
+// Now I will connect my MongoDB database to store the messages and
+// use that db to save and retrieve the messages that need to be posted.
+var mongoose  = require('mongoose')
 // now to host a static file I will use app.use() function
 
 app.use(express.static(__dirname)) // tells express we will be hosting a static file
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 
+// Now I will give my mongodb connection url. Since this is just a test
+// db I don't care that the password is online. But for future uses,
+// it needs to be stored in a config file somewhere safe.
+var dbUrl = "mongodb+srv://dbconnector:yk0BTK65RmKH5iiO@practicecluster.wasnv.mongodb.net/test"
 //now we create another variable called messages
 var messages = [
     {name: 'Tim', message: 'Hi'},
@@ -27,9 +41,17 @@ app.post('/messages', (req, res) => {
     // console.log(req.body)
     // so now instead of using the console.log we will use
     messages.push(req.body)
+    io.emit('message', req.body)
     res.sendStatus(200)
 })
 
+io.on('connection', (socket) => {
+    console.log('a user connected')
+})
+
+mongoose.connect(dbUrl, { useMongoClient: true }, (err) => {
+    console.log('mongo db connection', err)
+})
 
 // app is an instance of express and .listen tells it to listen on port 3000
 // Now lets set a call back on app.listen
